efb7fe6ea0c113ca2346bc8a81bdc52098a6999a
Namrata <snamrata4496@gmail.com>
2022-07-08 15:56:52 +0530

    Day 8

---
+++ Tree/Height-of-Binary-tree.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+};
+
+int height(node *node)
+{
+    if (node == NULL)
+        return 0;
+
+    int left = height(node->left);
+    int right = height(node->right);
+    int m = max(left, right);
+    return 1 + m;
+}
+
+node *newNode(int data)
+{
+    node *Node = new node();
+    Node->data = data;
+    Node->left = NULL;
+    Node->right = NULL;
+
+    return (Node);
+}
+
+int main()
+{
+    node *root = newNode(1);
+
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    cout << "Height of tree is " << height(root);
+    return 0;
+}
